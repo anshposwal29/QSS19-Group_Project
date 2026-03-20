64588667f69130f3f8e01844b86bdc3857380ac1
ida980 <idaly.barajas.28@dartmouth.edu>
2026-03-19 20:15:47 -0500

    Delete chapter1_intro.js

--- chapter1_intro.js
+++
@@ -1,7 +0,0 @@
-// cursor, only works in slide1 for some reason
-
-const cursorCircle = document.querySelector('.cursor-circle');
-
-document.addEventListener('mousemove', e => {
-  cursorCircle.style.left = `${e.clientX}px`;
-  cursorCircle.style.top  = `${e.clientY}px`;});
